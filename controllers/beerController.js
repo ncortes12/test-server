a7a5f5aba9dd4077742cacbcc3825b0642292129
ncortes12 <ncortes1415@gmail.com>
2018-10-09 15:02:18 -0700

    checking response

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -30,7 +30,8 @@ module.exports = {
     console.log(req)
     db.Beer
       .findAll({where:{BrewerId:req.body.id}})
-      .then(dbModel => {return res.json(dbModel)})
+      .then(dbModel => {return res.json(dbModel)
+      console.log(dbModel)})
       .catch(err => res.status(422).json(err));
 	},
 	
